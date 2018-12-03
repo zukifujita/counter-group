c67100cb61bf530af32e9d2ef202f99e1a476507
zjx-immersion <jianxin_home@163.com>
2018-12-03 16:20:24 +0800

    [Jason] feat: add a sum caculate from per counter increase and decrease

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -3,15 +3,17 @@ import React, { Component } from "react";
 class Counter extends Component {
   constructor(props) {
     super(props);
-    this.state = { count: 1 };
+    this.state = { count: 0 };
   }
-  
+
   increaseNumber = () => {
     this.setState({ count: this.state.count + 1 });
+    this.props.onCounterValueChanged(1);
   };
 
   decreaseNumber = () => {
     this.setState({ count: this.state.count - 1 });
+    this.props.onCounterValueChanged(-1);
   };
 
   render() {
--- src/component/CounterGroup.js
+++ src/component/CounterGroup.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import Counter from './Counter';
+import React, { Component } from "react";
+import Counter from "./Counter";
 
 class CounterGroup extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { counterCount: this.props.defaultCount }
+  constructor(props) {
+    super(props);
+    this.state = { counterCount: this.props.defaultCount, counterSum: 0 };
+  }
+
+  regenrateCounters = () => {
+    this.setState({ counterCount: this.refs.countInput.value, counterSum: 0 });
+  };
+
+  counterUpdateCallback = changedNum => {
+    this.setState({ counterSum: this.state.counterSum + changedNum });
+  };
+
+  render() {
+    let counters = [];
+    for (let count = 0; count < this.state.counterCount; count++) {
+      counters.push(
+        <Counter
+          key={count}
+          onCounterValueChanged={this.counterUpdateCallback}
+        />
+      );
     }
 
-    regenrateCounters = ()=>{
-        this.setState({counterCount: this.refs.countInput.value})
-    }
-
-    render() { 
-        let counters=[];
-        for(let count =0; count < this.state.counterCount; count ++){
-            counters.push(<Counter key={count}></Counter>)
-        }
-        
-        return ( <div>
-                {counters}
-                <input type="text" ref="countInput"></input>
-                <button onClick={this.regenrateCounters}>Regenerate indicated Counters</button>
-            </div> );
-    }
+    return (
+      <div>
+        {counters}
+        <input type="text" ref="countInput" />
+        <button onClick={this.regenrateCounters}>
+          Regenerate indicated Counters
+        </button>
+        <br />
+        <span>总和：{this.state.counterSum}</span>
+      </div>
+    );
+  }
 }
- 
+
 export default CounterGroup;
