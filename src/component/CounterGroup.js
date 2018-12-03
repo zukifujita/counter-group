c10b8e9ed07e5e4c48781d7352b974fb4f9ee411
zjx-immersion <jianxin_home@163.com>
2018-12-03 16:10:08 +0800

    [Jason] feat: add a regenerate counters button

--- src/component/CounterGroup.js
+++ src/component/CounterGroup.js
@@ -6,14 +6,21 @@ class CounterGroup extends Component {
         super(props);
         this.state = { counterCount: this.props.defaultCount }
     }
+
+    regenrateCounters = ()=>{
+        this.setState({counterCount: this.refs.countInput.value})
+    }
+
     render() { 
         let counters=[];
-        for(let count =0; count < this.props.defaultCount; count ++){
+        for(let count =0; count < this.state.counterCount; count ++){
             counters.push(<Counter key={count}></Counter>)
         }
-
+        
         return ( <div>
                 {counters}
+                <input type="text" ref="countInput"></input>
+                <button onClick={this.regenrateCounters}>Regenerate indicated Counters</button>
             </div> );
     }
 }
