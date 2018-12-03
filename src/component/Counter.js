e443d2abd9cd5663f1270226751636fe2360a7f5
zjx-immersion <jianxin_home@163.com>
2018-12-03 15:35:53 +0800

    [Jason] feat: a counter component implementation

---
+++ src/component/Counter.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 1 };
+  }
+  
+  increaseNumber = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decreaseNumber = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.increaseNumber}>+</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.decreaseNumber}>-</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
