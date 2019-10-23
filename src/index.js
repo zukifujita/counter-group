5e4c2700c146458fccca96cb4856b7eedabab67a
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-23 22:47:03 +0800

    Added index.js, store provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import CounterGroup from './component/CounterGroup';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import CounterGroup from "./container/CounterGroupContainer";
+import reducer from "./reducer";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
-ReactDOM.render(<CounterGroup defaultCount='3' />, document.getElementById('root'));
+const store = createStore(reducer);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Provider store={store}>
+        <CounterGroup defaultCount="3"/>
+    </Provider>,
+    document.getElementById("root")
+);
